799240f290f1c5ac6c1a83c06e3d40ecd481810f
danyPL <renatasiwiec71@gmail.com>
2024-06-14 13:25:51 +0200

    Dodano role dla uzytkownikow i wprowadzono middleware weryfikujacego dane role

--- server/Routes/UserRouter/User_Router.js
+++ server/Routes/UserRouter/User_Router.js
@@ -6,7 +6,7 @@ import { compare } from "bcrypt";
 import data from "../constants.js";
 import crypto from 'crypto'
 import { Controllers } from "./Controllers/controllers.js";
-import { Validate, Verify } from "../../utils/middlewares.js";
+import { Validate, Verify, VerifyRole } from "../../utils/middlewares.js";
 
 const userRouter = express.Router();
 const { Register,Login, Logout } = Controllers;
@@ -49,6 +49,13 @@ userRouter.post(
   Login
 );
 userRouter.get("/user", Verify, (req, res) => {
+  res.status(200).json({
+      status: "success",
+      message: "Welcome user!",
+  });
+  
+});
+userRouter.get("/admin/user", Verify,VerifyRole, (req, res) => {
   res.status(200).json({
       status: "success",
       message: "Welcome to the your Dashboard!",
--- server/utils/middlewares.js
+++ server/utils/middlewares.js
@@ -121,3 +121,34 @@ export const Verify = async (req, res, next) => {
       });
     }
   };
+  // można stworzyć osobne middleware dla każdej z roli albo przepuszczać przez next() odpowiednie komunikaty
+  export const VerifyRole = (req, res, next)=> {
+    try {
+        const user = req.user; // we have access to the user object from the request
+        const { role } = user; // extract the user role
+        // check if user has no advance privileges
+        // return an unathorized response
+        // tu sprawdzamy czy  ktoś jest adminem jeżeli tak przepuszczamy dalej
+        /*
+        nazewnictwo ról:
+        admin: 0x88
+        user: 0x01 
+
+
+        */
+        if (role !== "0x88") {
+            return res.status(401).json({
+                status: "failed",
+                message: "You are not authorized to view this page.",
+            });
+        }
+        next();
+    } catch (err) {
+        res.status(500).json({
+            status: "error",
+            code: 500,
+            data: [],
+            message: "Internal Server Error",
+        });
+    }
+}
