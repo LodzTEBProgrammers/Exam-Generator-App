fc178b8ca25f582f3881708112db3b76e9960377
Kuba Król <kuba.krol.ldz@gmail.com>
2024-07-12 15:03:02 +0200

    Praca nad Dashboardem ( Poprawki )

--- client/src/components/dashboard/dashboard.jsx
+++ client/src/components/dashboard/dashboard.jsx
@@ -1,28 +1,43 @@
 import Sidebar from "./sidebar/sidebar";
 import Exams  from "./exams/dataExam";
 import Exam from "./exams/Exam";
+import dataCalendaryExam from './exams/dataCalendary';
+import CalendaryExam from './exams/CalendaryExam';
 
 function Dashboard() {
   return (
     <>
       <div className="flex h-screen">
         <Sidebar />
-        <div className="flex flex-col flex-1 overflow-y-auto bg-[#0D0D0D] relative">
+        <div className="flex flex-col flex-1 overflow-y-auto bg-[#0D0D0D] relative pt-4 px-4">
             <div className="dashboard-content">
-              <section className="dashboard-boxes pt-4 px-4">
-                {/* Górna */}
                 <div className="bg-[#171717] w-full h-[532px] rounded-xl"></div>
-                {/* Trzy bloki */}
-                <div className="grid grid-cols-1 xl:grid-cols-3 mt-12 gap-16">
-                  <div className="dashboard-exams">
+                <div className="grid grid-cols-1 xl:grid-cols-3 mt-5 gap-8 xl:gap-16">
+                  <section className="dashboard-exams">
                     <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 flex-wrap">
                       {Exams.map((item, index) => (
                         <Exam key={index} {...item} />
                       ))}
                     </div>
-                  </div>
+                  </section>
+                  <section className="dashboard-calendary">
+                      <div className="flex justify-between">
+                        <p className="text-[#FFFFFF] text-[30px] font-bold">January</p>
+                        <div className="items-center justify-center flex">
+                          <button type="button" className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-2 rounded-full">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-calendar"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/></svg>
+                          </button>
+                        </div>
+                      </div>
+                      <div className="my-2 border-t-4 border-[#F1F5F9]/25 rounded-lg">
+                        <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 flex-wrap mt-6">
+                          {dataCalendaryExam.map((item, index) => (
+                            <CalendaryExam key={index} {...item} />
+                          ))}
+                        </div>
+                      </div>
+                  </section>
                 </div>
-              </section>
             </div>
         </div>
       </div>
--- client/src/components/dashboard/sidebar/sidebar.jsx
+++ client/src/components/dashboard/sidebar/sidebar.jsx
@@ -4,6 +4,7 @@ import Items from './utils/list';
 import ItemsMobile from './utils/listMobile';
 import SidebarItem from './utils/Sidebar_item';
 import SidebarItemMobile from './utils/Sidebar_itemMobile';
+import "./sidebar.css";
 
 const Sidebar = () => {
   const [isMobileView, setIsMobileView] = useState(false);
@@ -27,86 +28,28 @@ const Sidebar = () => {
             </div>
             <div className="flex items-center mt-auto justify-center mb-5">
               <div className="items-center justify-center flex">
-                <button
-                  type="button"
-                  className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-2 rounded-lg"
-                  onClick={toggleView}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1.5rem"
-                    height="1.5rem"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-panel-left-open"
-                  >
-                    <rect width="18" height="18" x="3" y="3" rx="2" />
-                    <path d="M9 3v18" />
-                    <path d="m14 9 3 3-3 3" />
-                  </svg>
+                <button type="button" className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-2 rounded-lg" onClick={toggleView}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-panel-left-open"> <rect width="18" height="18" x="3" y="3" rx="2" /> <path d="M9 3v18" /> <path d="m14 9 3 3-3 3" /></svg>
                 </button>
               </div>
             </div>
             <div className="w-full h-0.5 bg-[#D1D5DB]/10 mb-5"></div>
             <div className="flex items-center mt-auto justify-center">
               <div className="items-center justify-center flex mb-5 mr-3">
-                <a
-                  href=""
-                  className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-2 rounded-lg"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1.5rem"
-                    height="1.5rem"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-settings"
-                  >
-                    <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
-                    <circle cx="12" cy="12" r="3" />
-                  </svg>
+                <a href="" className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-2 rounded-lg">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-settings"><path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" /><circle cx="12" cy="12" r="3" /></svg>
                 </a>
               </div>
               <div className="items-center justify-center flex mb-5">
-                <a
-                  href=""
-                  className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-2 rounded-lg"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1.5rem"
-                    height="1.5rem"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-log-out"
-                  >
-                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-                    <polyline points="16 17 21 12 16 7" />
-                    <line x1="21" x2="9" y1="12" y2="12" />
-                  </svg>
+                <a href="" className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-2 rounded-lg">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline points="16 17 21 12 16 7" /><line x1="21" x2="9" y1="12" y2="12" /></svg>
                 </a>
               </div>
             </div>
             <div className="w-full h-0.5 bg-[#D1D5DB]/10"></div>
             <div className="mt-5 items-center justify-center flex">
               <div className="grid place-items-center items-center">
-                <img
-                  src="https://student.studentka.pl/img/wo/2/88/mezczyzna-obrazek_sredni_4025288.jpg"
-                  alt="Hero Obraz"
-                  className="max-w-full rounded-full h-12"
-                />
+                <img src="https://student.studentka.pl/img/wo/2/88/mezczyzna-obrazek_sredni_4025288.jpg" alt="Hero Obraz" className="max-w-full rounded-full h-12"/>
               </div>
             </div>
           </div>
@@ -117,29 +60,8 @@ const Sidebar = () => {
             <div className="flex items-center mb-auto">
               <img src={Traily} alt="Hero Obraz" className="max-w-full h-auto" />
               <div className="w-px h-6 bg-[#F1F5F9] opacity-15 mx-auto"></div>
-              {/* Przycisk Panelu Sidebar */}
-              <button
-                type="button"
-                className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-1 rounded-md"
-                onClick={toggleView}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="1.5rem"
-                  height="1.5rem"
-                  viewBox="0 0 24 24"
-                >
-                  <g
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                  >
-                    <rect width="18" height="18" x="3" y="3" rx="2" />
-                    <path d="M9 3v18m7-6l-3-3 3-3" />
-                  </g>
-                </svg>
+              <button type="button" className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-1 rounded-md" onClick={toggleView}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24"> <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"><rect width="18" height="18" x="3" y="3" rx="2" /><path d="M9 3v18m7-6l-3-3 3-3" /></g></svg>
               </button>
             </div>
             <div className="flex flex-col justify-center flex-grow gap-7">
@@ -149,58 +71,20 @@ const Sidebar = () => {
             </div>
             <div className="flex items-center mt-auto justify-center">
               <div className="items-center justify-center flex mb-5 mr-3">
-                <a
-                  href=""
-                  className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-2 rounded-lg"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1.5rem"
-                    height="1.5rem"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-settings"
-                  >
-                    <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
-                    <circle cx="12" cy="12" r="3" />
-                  </svg>
+                <a href="" className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-2 rounded-lg">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-settings"><path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" /><circle cx="12" cy="12" r="3" /></svg>
                 </a>
               </div>
               <div className="items-center justify-center flex mb-5">
-                <a
-                  href=""
-                  className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-2 rounded-lg"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1.5rem"
-                    height="1.5rem"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-log-out"
-                  >
-                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-                    <polyline points="16 17 21 12 16 7" />
-                    <line x1="21" x2="9" y1="12" y2="12" />
-                  </svg>
+                <a href="" className="text-[#D1D5DB] border-2 border-[#D1D5DB]/15 p-2 rounded-lg">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="1.5rem" height="1.5rem" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" /><polyline points="16 17 21 12 16 7" /><line x1="21" x2="9" y1="12" y2="12" /></svg>
                 </a>
               </div>
             </div>
             <div className="w-full h-0.5 bg-[#D1D5DB]/10"></div>
             <div className="mt-5 items-center justify-center flex">
               <div className="grid place-items-center items-center mr-4">
-                <img
-                  src="https://student.studentka.pl/img/wo/2/88/mezczyzna-obrazek_sredni_4025288.jpg"
-                  alt="Hero Obraz"
-                  className="max-w-full rounded-full h-12"
+                <img src="https://student.studentka.pl/img/wo/2/88/mezczyzna-obrazek_sredni_4025288.jpg" alt="Hero Obraz" className="max-w-full rounded-full h-12"
                 />
               </div>
               <p className="text-[#FFFFFF] text-[16px]">John Kowalski</p>
