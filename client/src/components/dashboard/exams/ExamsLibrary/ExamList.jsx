3c4ec6f79e363bcebd4eff58e62adee0db5a2106
Kalemak2 <152185330+Kalemak2@users.noreply.github.com>
2024-09-15 21:55:52 +0200

    Dodanie search baru + poprawki

--- client/src/components/dashboard/exams/ExamsLibrary/ExamList.jsx
+++ client/src/components/dashboard/exams/ExamsLibrary/ExamList.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React from 'react'
+import Exam from './Exam'
 
 const ExamList = () => {
   return (
-    <div className="flex flex-col text-[--Text-DarkMode] bg-[--BackgroundThird-DarkMode] p-4 m-2 rounded-xl">
-      <div className="flex flex-row w-full justify-between items-center">
-        <div className="flex flex-col">
-          <span className="font-bold text-2xl">Title</span>
-          <span className='text-[--TextSecond-DarkMode]'>Description</span>
+    <div>
+        <div className='flex justify-between text-[--TextSecond-DarkMode] items-center m-2 border-b border-[--TextThird-DarkMode] p-4 m-4'>
+            
+            <div className='flex bg-[--TextThird-DarkMode] rounded-md items-center'>
+                <svg className='mx-2' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
+                <input type="text" placeholder='Search' className='bg-[--TextThird-DarkMode] placeholder-[--TextSecond-DarkMode] py-2 pr-3 pl-1 rounded-md border-transparent'/>
+            </div>
+            <div className='flex space-x-3'>
+                <div className='flex space-x-1 border-2 rounded-full py-2 px-3 cursor-pointer tracking-wider border-[#F1F5F9]/15 '>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-down"><path d="m21 16-4 4-4-4"/><path d="M17 20V4"/><path d="m3 8 4-4 4 4"/><path d="M7 4v16"/></svg>
+                    <span>Sort by</span>
+                </div>
+                <div className='flex space-x-1 border-2 rounded-full py-2 px-3 cursor-pointer tracking-wider border-[#F1F5F9]/15'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chart-no-axes-column"><line x1="18" x2="18" y1="20" y2="10"/><line x1="12" x2="12" y1="20" y2="4"/><line x1="6" x2="6" y1="20" y2="14"/></svg>
+                <span>Filters</span>
+                </div>
+            </div>
         </div>
-        <div className="flex space-x-4">
-          <div className='flex space-x-3 border-2 rounded-lg py-2 px-3 cursor-pointer tracking-wider border-[--TextFourth-DarkMode]'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-square-pen"><path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"/></svg>
-          <span>Edit</span>
-          </div>
-          <div className='border-2 rounded-lg py-2 px-3 cursor-pointer tracking-wider border-[--TextFourth-DarkMode]'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
-          </div>
+        <div>
+            <Exam/>
         </div>
-      </div>
     </div>
-  );
-};
+  )
+}
 
-export default ExamList;
+export default ExamList
